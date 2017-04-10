ed271d7a1b50db1e900e4f212b6d26d19f44173e
alm <ayarnykh@rustest.ru>
2017-04-10 14:06:08 +0300

    first commit

---
+++ InnerService/InnerService.cs
@@ -0,0 +1,103 @@
+﻿using System;
+using System.Diagnostics;
+using System.ServiceProcess;
+
+namespace InnerService
+{
+    class InnerService : ServiceBase
+    {
+        public InnerService()
+        {
+            this.ServiceName = "Inner Service";
+            this.EventLog.Log = "Application";
+
+            this.CanHandlePowerEvent = false;
+            this.CanHandleSessionChangeEvent = false;
+            this.CanPauseAndContinue = true;
+            this.CanShutdown = true;
+            this.CanStop = true;
+        }
+
+        static void Main()
+        {
+            ServiceBase.Run(new InnerService());
+        }
+
+        protected override void Dispose(bool disposing)
+        {
+            base.Dispose(disposing);
+        }
+
+        protected override void OnStart(string[] args)
+        {
+            base.OnStart(args);
+            
+        }
+
+        protected override void OnStop()
+        {
+            base.OnStop();
+        }
+
+        protected override void OnPause()
+        {
+            base.OnPause();
+        }
+
+        protected override void OnContinue()
+        {
+            base.OnContinue();
+        }
+
+        protected override void OnShutdown()
+        {
+            base.OnShutdown();
+        }
+
+        /// <summary>
+        /// OnCustomCommand(): If you need to send a command to your
+        ///   service without the need for Remoting or Sockets, use
+        ///   this method to do custom methods.
+        /// </summary>
+        /// <param name="command">Arbitrary Integer between 128 & 256</param>
+        protected override void OnCustomCommand(int command)
+        {
+            //  A custom command can be sent to a service by using this method:
+            //#  int command = 128; //Some Arbitrary number between 128 & 256
+            //#  ServiceController sc = new ServiceController("NameOfService");
+            //#  sc.ExecuteCommand(command);
+
+            base.OnCustomCommand(command);
+        }
+
+        protected override bool OnPowerEvent(PowerBroadcastStatus powerStatus)
+        {
+            return base.OnPowerEvent(powerStatus);
+        }
+
+        /// <summary>
+        /// OnSessionChange(): To handle a change event
+        ///   from a Terminal Server session.
+        ///   Useful if you need to determine
+        ///   when a user logs in remotely or logs off,
+        ///   or when someone logs into the console.
+        /// </summary>
+        /// <param name="changeDescription">The Session Change
+        /// Event that occured.</param>
+        protected override void OnSessionChange(
+                  SessionChangeDescription changeDescription)
+        {
+            base.OnSessionChange(changeDescription);
+        }
+
+        private void LogEvent(string message)
+        {
+            string eventSource = "File Monitor and Send Service";
+            DateTime dt = new DateTime();
+            dt = System.DateTime.UtcNow;
+            message = dt.ToLocalTime() + ": " + message;
+
+            EventLog.WriteEntry(eventSource, message);
+        }
+    }
+}
---
+++ InnerService/InnerServiceInstaller.cs
@@ -0,0 +1,29 @@
+﻿using System.ComponentModel;
+using System.Configuration.Install;
+using System.ServiceProcess;
+
+namespace InnerService
+{
+    [RunInstaller(true)]
+    public class InnerServiceInstaller : Installer
+    {
+        public InnerServiceInstaller()
+        {
+            ServiceProcessInstaller serviceProcessInstaller =
+                               new ServiceProcessInstaller();
+            ServiceInstaller serviceInstaller = new ServiceInstaller();
+
+            serviceProcessInstaller.Account = ServiceAccount.LocalSystem;
+            serviceProcessInstaller.Username = null;
+            serviceProcessInstaller.Password = null;
+
+            serviceInstaller.DisplayName = "Inner Service";
+            serviceInstaller.StartType = ServiceStartMode.Automatic;
+
+            serviceInstaller.ServiceName = "Inner Service";
+
+            this.Installers.Add(serviceProcessInstaller);
+            this.Installers.Add(serviceInstaller);
+        }
+    }
+}
